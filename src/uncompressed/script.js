e772992e1f0433915fd66bb8b19ea09ba7fc81eb
DBProductions <dennis@dbproductions.de>
2012-10-11 20:51:03 +0200

    remove unused code

--- src/uncompressed/script.js
+++ src/uncompressed/script.js
@@ -14,19 +14,18 @@ JSHELPING.Core.getModule = function(name) {
 };
 JSHELPING.Core.loadModule = function(name) {
     var s = document.createElement('script');
-	s.async = true;
-	s.src = 'src/' + name + '-min.js';
-	document.getElementsByTagName('head')[0].appendChild(s);
-	var that = this;
-	s.onload = function() {
-	    JSHELPING.Core.register(name, JSHELPING.loadedmodule[name]);
-	}	
+    s.async = true;
+    s.src = 'src/' + name + '-min.js';
+    document.getElementsByTagName('head')[0].appendChild(s);
+    s.onload = function() {
+        JSHELPING.Core.register(name, JSHELPING.loadedmodule[name]);
+    }	
 };
 JSHELPING.Core.init = (function() {
-	var scripts = document.getElementsByTagName('script');
-	var queryString = scripts[scripts.length - 1].src.replace(/^[^\?]+\??/,'');
-	var modules = queryString.split(',');
-	for(var i = 0; i < modules.length; i++) {
-		JSHELPING.Core.loadModule(modules[i]);
-	}
+    var scripts = document.getElementsByTagName('script');
+    var queryString = scripts[scripts.length - 1].src.replace(/^[^\?]+\??/,'');
+    var modules = queryString.split(',');
+    for(var i = 0; i < modules.length; i++) {
+        JSHELPING.Core.loadModule(modules[i]);
+    }
 }());
