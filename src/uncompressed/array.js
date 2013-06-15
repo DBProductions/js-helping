5b00eb938e5b9da23e1f1951a0849594da9ee4c4
DBProductions <dennis@dbproductions.de>
2013-06-16 03:22:14 +0800

    update isArray method and add equalArray method

--- src/uncompressed/array.js
+++ src/uncompressed/array.js
@@ -1,14 +1,25 @@
 if(!JSHELPING) {
     var JSHELPING = {};
 }
-JSHELPING._array = function() {
+JSHELPING._array = function _array() {
     'use strict';
     return {
         isArray: function(testObj) {
-            return testObj && !(testObj.propertyIsEnumerable('length')) && typeof testObj === 'object' && typeof testObj.length === 'number';
+            return (Object.prototype.toString.call(testObj) === '[object Array]');
         },
-        each: function(arr, method) {        
-            for (var i = arr.length-1; i >= 0; i--) {
+        equalArray: function(array1, array2) {
+            if (array1.length !== array2.length) {
+                return false;
+            }
+            var areEqual = true, i;
+            for (i = 0; (i < array2.length) && areEqual; i++) {
+                areEqual = (array1[i] === array2[i]);
+            }
+            return areEqual;
+        },
+        each: function(arr, method) {
+            var i;        
+            for (i = arr.length-1; i >= 0; i--) {
                 arr[i] = method(arr[i]);
             }
         },
--- tests/qunitTest.js
+++ tests/qunitTest.js
@@ -9,23 +9,30 @@ window.onload = function() {
         var testArr = [1,2,3], 
             testNum = 3, 
             testStr = 'test';
-        equal( true, modArray.isArray(testArr), "returns true it's an array" );
-        equal( false, modArray.isArray(testNum), "returns false it's a number" );
-        equal( false, modArray.isArray(testStr), "returns false it's a string" );			
+        equal(true, modArray.isArray(testArr), "returns true it's an array");
+        equal(false, modArray.isArray(testNum), "returns false it's a number");
+        equal(false, modArray.isArray(testStr), "returns false it's a string");			
+    });
+    test("equalArray", function() {
+        var testArr1 = [1,2,3];
+        var testArr2 = [1,2,3];
+        equal(true, modArray.equalArray(testArr1, testArr2), "returns true arrays are equal");
+        testArr2.shift();
+        equal(false, modArray.equalArray(testArr1, testArr2), "returns false arrays are not equal");
     });
     test("each and remove", function() {
         expect(6);
         function countup(x) { return x += 1; }
         var arr = [1,2,3,4,5];
         modArray.each(arr, countup);
-        equal( 2, arr[0], "returns right value" );
-        equal( 4, arr[2], "returns right value" );
-        equal( 6, arr[4], "returns right value" );
+        equal(2, arr[0], "returns right value");
+        equal(4, arr[2], "returns right value");
+        equal(6, arr[4], "returns right value");
         arr = modArray.remove(arr, 6);
-        equal( 5, arr[3], "last element has right value" );
-        equal( 4, arr.length, "right length" );
+        equal(5, arr[3], "last element has right value");
+        equal(4, arr.length, "right length");
         arr.push(7);
-        equal( 5, arr.length, "right length" );
+        equal(5, arr.length, "right length");
     });
 
     module("JSHELPING element module");
@@ -34,26 +41,26 @@ window.onload = function() {
         expect(2);
         modElement.add('#qunit', 'newTestEle');
         var ele = document.querySelector('#newTestEle');
-        equal( "object", typeof(ele), "add element" );
+        equal("object", typeof(ele), "add element");
         modElement.remove('newTestEle');
         ele = document.querySelector('#newTestEle');
-        equal( null, ele, "remove element" );
+        equal(null, ele, "remove element");
     });
     test("add class and has class testing", function() {
         expect(2);
         var ele = document.querySelector('#qunit');
         modElement.addClass(document.querySelector('#qunit'), 'test1');
         modElement.addClass(document.querySelector('#qunit'), 'test2');
-        equal( true, modElement.hasClass(ele, 'test1'), "add class test1" );
-        equal( true, modElement.hasClass(ele, 'test2'), "add class test2" );
+        equal(true, modElement.hasClass(ele, 'test1'), "add class test1");
+        equal(true, modElement.hasClass(ele, 'test2'), "add class test2");
     });
     test("remove class and has class testing", function() {
         expect(2);
         var ele = document.querySelector('#qunit');
         modElement.removeClass(document.querySelector('#qunit'), 'test1');
         modElement.removeClass(document.querySelector('#qunit'), 'test2');
-        equal( false, modElement.hasClass(ele, 'test1'), "remove class test1" );
-        equal( false, modElement.hasClass(ele, 'test2'), "remove class test2" );
+        equal(false, modElement.hasClass(ele, 'test1'), "remove class test1");
+        equal(false, modElement.hasClass(ele, 'test2'), "remove class test2");
     });
 
     module("JSHELPING object module");
@@ -70,15 +77,15 @@ window.onload = function() {
     });
     test("extend an object", function() {
         expect(4);
-        var P = function() {
+        var P = function P() {
             this.name = 'value';
             this.getName = function() {
                 return this.name;
             }
         };
-        var C = function() {};
+        var C = function C() {};
         modObject.extend(C, P);
-        var D = function() {};
+        var D = function D() {};
         modObject.extend(D, C);
         var p = new P();
         var c = new C();
